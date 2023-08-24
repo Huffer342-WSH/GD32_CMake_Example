1256627063a392045b624af64149cf9d59d25929
Huffer342-WSH <718007138@qq.com>
2023-08-24 23:48:49 +0800

    添加rtt测试代码

--- Core/src/test.c
+++ Core/src/test.c
@@ -2,15 +2,39 @@
 #define _TEST_H_
 
 #include "test.h"
+#include "systick.h"
 #include "uart.h"
 #include "log.h"
+#include "string.h"
+#include "SEGGER_RTT.h"
+
 static char str0[] = "Uart test\r\n";
 static char str1[] = "Control uart transmission by register mode\r\n";
 static char str2[] = "Send data from uart by printf function\r\n";
 static char buf[128];
+
+void RTT_test() {
+    static uint8_t init_flag = 0;
+    // 初始化SEGGER RTT
+    if (!init_flag) {
+        SEGGER_RTT_Init();
+        init_flag = 1;
+    }
+
+    SEGGER_RTT_printf(0, "Here is Segger RTT default channel\r\n");
+    for (int i = 0; i < 5; i++) {
+        delay_ms(1000);
+        SEGGER_RTT_printf(0, "Second %d\r\n", i);
+    }
+}
+
 void uart_test() {
+    static uint8_t init_flag = 0;
     // 初始化uart
-    Usartx_Init();
+    if (!init_flag) {
+        Usartx_Init();
+        init_flag = 1;
+    }
 
     LOG("%s", str0);
 
@@ -34,7 +58,13 @@ void uart_test() {
     while (1) {
         if (!USART1_Get_Rx(&rx_str, &rx_size)) {
             LOG("uart received message:\r\n%s\r\n----- END -----", rx_str);
+            if (rx_size >= 8 && !memcmp(rx_str, "RTT test", 8)) {
+                LOG("RTT_test start.");
+                RTT_test();
+                LOG("RTT_test end.");
+            }
         }
     }
 }
+
 #endif /* _TEST_H_ */
